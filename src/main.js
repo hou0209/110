3e122fdf1eeed27ba39c510ebfcb592e33718251
hou0209 <39614777+hou0209@users.noreply.github.com>
2019-02-19 23:28:14 +0800

    app-sidebar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+//基于webpack的开发，是基于模块化的开发
+//var Vue = require('vue);
+import Vue from 'vue';
+import App from './App.vue';
+//导入全局的样式
+//导入bootstarp的样式
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/index.css';
 
 new Vue({
   el: '#app',
